7bb1c9b364fc470b7020b465fb6b72f7047f841d
ArtAboveReality <info@artabovereality.com>
2022-06-17 20:41:17 -0700

    Initial Commit

---
+++ ArtAboveReality Interface App/ArtAboveReality_Interface_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtAboveReality_Interface_AppApp.swift
+//  ArtAboveReality Interface App
+//
+//  Created by Badir Mccleary on 6/17/22.
+//
+
+import SwiftUI
+
+@main
+struct ArtAboveReality_Interface_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
