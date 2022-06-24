7930657b7dc4b7233229747ac26749895ee6d690
Dwayne Langley <Dwayne.Langley@gmail.com>
2022-06-23 22:21:44 -0700

    Initial Commit

---
+++ Mobile/MobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileApp.swift
+//  Mobile
+//
+//  Created by Dwayne Langley on 6/23/22.
+//
+
+import SwiftUI
+
+@main
+struct MobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
